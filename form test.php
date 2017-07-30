31ab22301e2e3e89f03f80d79eec86b3d406d4ba
Spaghettismmm <revengeofthemagnets@yahoo.com>
2017-07-30 17:08:20 -0500

    WMACart
    Initial upload

---
+++ form test.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+
+</head>
+<body>	
+
+
+<?php 
+				$firstname= $lastname= $email= "";
+				
+				
+			if (isset($_REQUEST['submitted'])) {
+					// Initialize error array.
+					  $errors = array();
+					  // Check for a proper First name
+				  if (!empty($_REQUEST['firstname'])) {
+					  $firstname = $_REQUEST['firstname'];
+						  if (preg_match("/^[a-zA-Z]{1,20}+$/",$firstname)){ $firstname = $_REQUEST['firstname'];}
+						  else{ $errors[] = 'Your Name can only contain A-Z or a-z, 1-20 characters long.';}
+				  } else {$errors[] = 'You forgot to enter your First Name.';}
+				  
+				  // Check for a proper Last name
+				  if (!empty($_REQUEST['lastname'])) {
+				  $lastname = $_REQUEST['lastname'];
+				  if (preg_match("/^[a-zA-Z]{1,20}+$/",$lastname)){ $lastname = $_REQUEST['lastname'];}
+				  else{ $errors[] = 'Your Name can only contain A-Z or a-z, 1-20 characters long.';}
+				  } else {$errors[] = 'You forgot to enter your Last Name.';}
+				  
+				  //Check for a valid email number
+					if (!empty($_REQUEST['email'])) {
+					  $email = $_REQUEST['email'];
+								$pattern = "/^[\w\+\-\.\~]+\@[\-\w\.\!]+$/";
+					  if (preg_match($pattern,$email)){ $email = $_REQUEST['email'];
+					}
+					else{ $errors[] = 'Your email number can not contain that character.';}
+					} 
+					else {$errors[] = 'You forgot to enter your email.';
+					}
+			}
+			 
+			  //End of validation 
+			  
+				if (isset($_REQUEST['submitted'])) {
+					  if (empty($errors)) { 
+					  $from = "From: WM Art Contact us"; //Site name
+					  // Change this to your email address you want to form sent to
+					  $to = "willmac321@gmail.com"; 
+					  $name=$firstname. ' ' .$lastname;
+					  $subject = "WMAC ART Comment from " . $name ;
+					  
+					  $message = "Message from " .$firstname. " " . $lastname ; 
+					  "Email/Phone: " . $email ;
+					 /**mail($to,$subject,$message,$from);**/
+					  }
+				}
+		
+?>
+
+<?php 
+				  //Print Errors
+				if (isset($_REQUEST["submitted"])) {
+				  // Print any error messages. 
+					  if (!empty($errors)) { 
+					  echo '<hr/><h3>The following occurred:</h3><ul>'; 
+					  // Print each error. 
+					  foreach ($errors as $msg) { echo '<li>'. $msg . '</li>';}
+					  echo '</ul><h3>Your mail could not be sent due to input errors.</h3><hr/>';}
+					   else{echo '<hr/><h3 align="center">Your mail was sent. Thank you!</h3>
+					   <hr/><p4>Below is the message that you sent.</p4>'; 
+					  echo "Message from " . $firstname . " " . $lastname . " <br />Email/Phone: ".$email." <br />";
+					  }
+				}
+				//End of errors array
+?> 
+
+		
+		
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			  First name:<br>
+			  <input type="text" name="firstname" value='<?php echo $firstname;?>'/>
+			  <br>
+			  Last name:<br>
+			  <input type="text" name="lastname" value='<?php echo $lastname;?>' />
+			  <br>
+			  Contact Email/Phone:<br>
+			  <input type="text" name="email" value='<?php echo $email;?>'/>
+			  <br>
+			  Comments:<br>
+			  <textarea name="comment" style="height:200px" ><?php echo htmlspecialchars($comment);?></textarea>
+			  
+			  <br><br>
+			  <input type="submit" name="submitted" value="Submit" />
+		</form>  
+</body>
+
+</html>    
